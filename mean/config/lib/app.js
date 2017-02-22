c7aa48de276eccd3bb0c8deffd52805d96d9c782
Will Norman <willnorman@users.noreply.github.com>
2017-02-22 00:06:19 -0500

    Revert "First demo"

--- mean/config/lib/app.js
+++ mean/config/lib/app.js
@@ -1,61 +1,57 @@
-// public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService']);
+'use strict';
 
+/**
+ * Module dependencies.
+ */
+var config = require('../config'),
+  mongoose = require('./mongoose'),
+  express = require('./express'),
+  chalk = require('chalk'),
+  seed = require('./seed');
 
-// 'use strict';
+function seedDB() {
+  if (config.seedDB && config.seedDB.seed) {
+    console.log(chalk.bold.red('Warning:  Database seeding is turned on'));
+    seed.start();
+  }
+}
 
-// /**
-//  * Module dependencies.
-//  */
-// var config = require('../config'),
-//   mongoose = require('./mongoose'),
-//   express = require('./express'),
-//   chalk = require('chalk'),
-//   seed = require('./seed');
+// Initialize Models
+mongoose.loadModels(seedDB);
 
-// function seedDB() {
-//   if (config.seedDB && config.seedDB.seed) {
-//     console.log(chalk.bold.red('Warning:  Database seeding is turned on'));
-//     seed.start();
-//   }
-// }
+module.exports.init = function init(callback) {
+  mongoose.connect(function (db) {
+    // Initialize express
+    var app = express.init(db);
+    if (callback) callback(app, db, config);
 
-// // Initialize Models
-// mongoose.loadModels(seedDB);
+  });
+};
 
-// module.exports.init = function init(callback) {
-//   mongoose.connect(function (db) {
-//     // Initialize express
-//     var app = express.init(db);
-//     if (callback) callback(app, db, config);
+module.exports.start = function start(callback) {
+  var _this = this;
 
-//   });
-// };
+  _this.init(function (app, db, config) {
 
-// module.exports.start = function start(callback) {
-//   var _this = this;
+    // Start the app by listening on <port> at <host>
+    app.listen(config.port, config.host, function () {
+      // Create server URL
+      var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
+      // Logging initialization
+      console.log('--');
+      console.log(chalk.green(config.app.title));
+      console.log();
+      console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
+      console.log(chalk.green('Server:          ' + server));
+      console.log(chalk.green('Database:        ' + config.db.uri));
+      console.log(chalk.green('App version:     ' + config.meanjs.version));
+      if (config.meanjs['meanjs-version'])
+        console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
+      console.log('--');
 
-//   _this.init(function (app, db, config) {
+      if (callback) callback(app, db, config);
+    });
 
-//     // Start the app by listening on <port> at <host>
-//     app.listen(config.port, config.host, function () {
-//       // Create server URL
-//       var server = (process.env.NODE_ENV === 'secure' ? 'https://' : 'http://') + config.host + ':' + config.port;
-//       // Logging initialization
-//       console.log('--');
-//       console.log(chalk.green(config.app.title));
-//       console.log();
-//       console.log(chalk.green('Environment:     ' + process.env.NODE_ENV));
-//       console.log(chalk.green('Server:          ' + server));
-//       console.log(chalk.green('Database:        ' + config.db.uri));
-//       console.log(chalk.green('App version:     ' + config.meanjs.version));
-//       if (config.meanjs['meanjs-version'])
-//         console.log(chalk.green('MEAN.JS version: ' + config.meanjs['meanjs-version']));
-//       console.log('--');
+  });
 
-//       if (callback) callback(app, db, config);
-//     });
-
-//   });
-
-// };
+};
