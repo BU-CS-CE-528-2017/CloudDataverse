d413872f246fdaa06c471ee51bd85185588a95eb
Benjamin Corn <bcorn@bu.edu>
2017-02-22 00:56:34 -0500

    Update JSTACK Core

--- mean/config/assets/production.js
+++ mean/config/assets/production.js
@@ -18,7 +18,7 @@ module.exports = {
 	'public/jstack/JSTACK.Cinder.js',
 	'public/jstack/JSTACK.Comm.js',
 	'public/jstack/JSTACK.Glance.js',
-	'public/jstack/JSTACK.JSTACK.js',
+	'public/jstack/JSTACK.js',
 	'public/jstack/JSTACK.Keystone.js',
 	'public/jstack/JSTACK.Murano.js',
 	'public/jstack/JSTACK.Neutron.js',
