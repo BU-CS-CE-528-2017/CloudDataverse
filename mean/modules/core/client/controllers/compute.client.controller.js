3812610e1a3d211146020e0f4fb6668c61716737
Sneha Pradhan <snehha@bu.edu>
2017-03-10 15:47:28 -0500

    indentations

--- mean/modules/core/client/controllers/compute.client.controller.js
+++ mean/modules/core/client/controllers/compute.client.controller.js
@@ -1,25 +1,22 @@
 (function () {
-  'use strict';
-  
+  'use strict';  
   angular
     .module('core')
     .controller('ComputeController', ComputeController);
-  
   function ComputeController($scope, $http) {
     var vm = this;
     vm.ServerList = {};
     $http.get('/api/list/servers')
       .then(function (res) {
         vm.ServerList = res.data;
-  
         $http.get('/api/list/flavors')
           .then(function (res) {
             vm.Flavors = res.data;
             $http.get('/api/list/images')
               .then(function (res) {
                 vm.Images = res.data;
-              })
-          })
-      })
+              });
+          });
+      });
   }
 }());
