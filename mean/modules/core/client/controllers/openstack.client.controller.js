377434ec6a1aafa8305b879adeb1005d009ed67b
Sneha Pradhan <snehha@bu.edu>
2017-03-10 16:07:31 -0500

    indentations

--- mean/modules/core/client/controllers/openstack.client.controller.js
+++ mean/modules/core/client/controllers/openstack.client.controller.js
@@ -1,25 +1,24 @@
 (function () {
-  'use stri  ct';
+  'use strict';
 
   angular
     .module('core')
     .controller('OpenStackController', OpenStackController);
-  
+
   function OpenStackController($scope, $http) {
     var vm = this;
     vm.User = {};
-    vm.User.UserName = "";
-    vm.User.Password = "";
-    vm.LoginWarning = "";
+    vm.User.UserName = '';
+    vm.User.Password = '';
+    vm.LoginWarning = '';
     vm.WarningFlag = false;
 
     vm.AuthenticateUser = function () {
       // empty fields, don't post
       if (vm.User.UserName.length === 0 || vm.User.Password.length === 0) {
-        vm.LoginWarning = "Username or Password is missing.";
+        vm.LoginWarning = 'Username or Password is missing.';
         vm.WarningFlag = true;
-    
-          // focus form field on empty field
+        // focus form field on empty field
         if (!vm.User.UserName.length)
           $('#open-stack-username').focus();
         else
@@ -27,11 +26,11 @@
       } else {
           // make post to node auth endpoint
         var req = { user: vm.User.UserName, password: vm.User.Password };
-        $http.post("/api/auth", req)
+        $http.post('/api/auth', req)
           .then(function successCallback (res) {
-            window.location = "/compute";
+            window.location = '/compute';
           }, function errorCallback(res) {
-            vm.LoginWarning = "Oops... we couldn't authenticate you. Try again.";
+            vm.LoginWarning = 'Oops... we couldn\'t authenticate you. Try again.';
             vm.WarningFlag = true;
           });
       }
