5003e0e0c6d9e4006a4c8aa6345c81c532714e51
Benjamin Corn <bcorn@bu.edu>
2017-03-12 17:05:27 -0400

    Basic Mode UI for Launching Cluster
    "Basic Mode" allows users to choose from the three plugins available and
    choose how many nodes they want and then launch the cluster. This does
    not include the "Advanced" option yet.

--- mean/modules/core/client/controllers/compute.client.controller.js
+++ mean/modules/core/client/controllers/compute.client.controller.js
@@ -1,22 +1,47 @@
 (function () {
-  'use strict';
-  angular
-    .module('core')
-    .controller('ComputeController', ComputeController);
-  function ComputeController($scope, $http) {
-    var vm = this;
-    vm.ServerList = {};
-    $http.get('/api/list/servers')
-      .then(function (res) {
-        vm.ServerList = res.data;
-        $http.get('/api/list/flavors')
+    'use strict';
+    angular
+      .module('core')
+      .controller('ComputeController', ComputeController);
+    function ComputeController($scope, $http) {
+        var vm = this;
+        vm.ServerList = {};
+        vm.Cluster = {};
+        vm.Cluster.NodeCount = 2;
+        vm.Plugins = [{ id: 1, name: 'Hadoop' }, { id: 2, name: 'Storm' }, { id: 3, name: 'Spark' }];
+
+        vm.VerifyClusterCount = function () {
+            if (vm.Cluster.InstanceCount < 0) {
+                vm.Cluster.InstanceCount = 1;
+                vm.Cluster.NodeCount = 0;
+            }
+            else if (vm.Cluster.InstanceCount === undefined) {
+                vm.Cluster.NodeCount = 0;
+            }
+            else {
+                vm.Cluster.NodeCount = vm.Cluster.InstanceCount - 1;
+            }
+
+        }
+
+        vm.LaunchCluster = function () {
+
+        }
+
+        $http.get('/api/list/servers')
           .then(function (res) {
-            vm.Flavors = res.data;
-            $http.get('/api/list/images')
-              .then(function (res) {
-                vm.Images = res.data;
-              });
+              vm.ServerList = res.data;
+              $http.get('/api/list/flavors')
+                .then(function (res) {
+                    vm.Flavors = res.data;
+
+                    if (!vm.Flavors.length)
+                        document.location.href = "/";
+                    //$http.get('/api/list/images')
+                    //  .then(function (res) {
+                    //      vm.Images = res.data;
+                    //  });
+                });
           });
-      });
-  }
+    }
 }());
