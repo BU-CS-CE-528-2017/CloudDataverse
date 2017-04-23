7a70b74dae1a9f13f870a56f2cb32cf4addfc213
Benjamin Corn <bcorn@bu.edu>
2017-04-23 16:10:55 -0400

    [Patch] Display Warning to User if Cluster Name is Empty When Uploading Job Binary

--- mean/modules/core/client/controllers/compute.client.controller.js
+++ mean/modules/core/client/controllers/compute.client.controller.js
@@ -1,218 +1,237 @@
-(function() {
-  'use strict';
-  angular
-    .module('core')
-    .controller('ComputeController', ComputeController);
+(function () {
+    'use strict';
+    angular
+      .module('core')
+      .controller('ComputeController', ComputeController);
 
-  function ComputeController($scope, $http) {
-    var vm = this;
-    vm.ConfigMode = 0;
-    var binaryUrl;
-    vm.BinaryUrl = '';
-    vm.ServerList = {};
-    vm.Cluster = {};
-    vm.Cluster.NodeCount = 2;
-    var statusUpdater;
+    function ComputeController($scope, $http) {
+        var vm = this;
 
-    vm.nextTab = function () {
-        $('.compute-pill').trigger('click')
-    }
+        // Config
+        vm.ConfigMode = 0;
+        vm.BinaryUrl = '';
+        vm.ServerList = {};
 
-    vm.UploadBinary = function () {
-        var file = $('#upload-input').get(0).files[0];
-        vm.BinaryFileName = file.name;
+        // Cluster Config Init
+        vm.Cluster = {};
+        vm.Cluster.Name = '';
+        vm.Cluster.NodeCount = 2;
 
-        if (file) {
-            var formData = new FormData();
-            formData.append('uploads[]', file, vm.Cluster.Name + '/' + file.name);
+        // Cluster (From Template) Config Init
+        vm.ClusterFT = {};
+        vm.ClusterFT.Name = '';
 
-            $.ajax({
-                url: '/api/upload/binary',
-                type: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function (data) {
-                    vm.BinaryUrl = data;
-                    $scope.$apply();
-                }
-            });
+        var statusUpdater;
+        var binaryUrl;
+
+        vm.nextTab = function () {
+            $('.compute-pill').trigger('click')
         }
-    };
 
-    vm.CreateJob = function () {
-        var job = {
-            'job_type': 'MapReduce',
-            'container_name': vm.Cluster.Name,
-            'cluster_id': vm.ClusterDetails.id,
-            'input_sources': vm.SelectedFiles,
-            'binary_url': vm.Cluster.Name + '/' + vm.BinaryFileName
-        };
+        vm.UploadBinary = function () {
 
-        $http.post('/api/create/data_job', job)
-          .then(function (res) {
-              vm.Job = res.data.job_execution;
-              statusUpdater = setInterval(updateJobStatus, 1000);
-          });
-    };
-
-    vm.VerifyClusterCount = function() {
-      if (vm.Cluster.InstanceCount <= 0) {
-        vm.Cluster.InstanceCount = 1;
-        vm.Cluster.NodeCount = 0;
-      } else if (vm.Cluster.InstanceCount === undefined) {
-        vm.Cluster.NodeCount = 0;
-      } else {
-        vm.Cluster.NodeCount = vm.Cluster.InstanceCount - 1;
-      }
-    };
-
-    vm.LaunchCluster = function() {
-
-      var launchClusterPayload = {
-        'plugin_name': vm.Cluster.Plugin,
-        'name': vm.Cluster.Name,
-        'count': vm.Cluster.InstanceCount - 1,
-        'user_keypair_id': vm.Cluster.KeyPair,
-        'flavor': vm.Cluster.Flavor,
-        'network': vm.Cluster.Network
-      };
-
-      $http.post('/api/launch', launchClusterPayload)
-        .then(function(res) {
-          if (res.data.error_name !== undefined) {
-            vm.LaunchSuccess = false;
-            vm.ClusterDetails = res.data;
-          } else {
-            vm.LaunchSuccess = true;
-            vm.ClusterDetails = res.data.cluster;
-
-            statusUpdater = setInterval(updateLaunchStatus, 5000);
-          }
-        });
-    };
-
-    vm.LaunchFT = function () {
-        var launchClusterPayload = {
-            'plugin_name': vm.SelectedClusterTemplate.plugin_name,
-            'template_id': vm.SelectedClusterTemplate.id,
-            'name': vm.ClusterFT.Name,
-            'user_keypair_id': vm.ClusterFT.KeyPair,
-            'network': vm.ClusterFT.Network
-        };
-
-        $http.post('/api/create/cluster_from_template', launchClusterPayload)
-          .then(function (res) {
-              if (res.data.error_name !== undefined) {
-                  vm.LaunchSuccess = false;
-                  vm.ClusterDetails = res.data;
-              } else {
-                  vm.LaunchSuccess = true;
-                  vm.ClusterDetails = res.data.cluster;
-
-                  statusUpdater = setInterval(updateLaunchStatus, 5000);
-              }
-          });
-    }
-
-    var updateJobStatus = function () {
-        $http.get('/api/status/job/' + vm.Job.id)
-            .then(function (res) {
-                var resp = JSON.parse(res.data);
-                vm.Job.info = resp.job_execution.info;
-
-                if (vm.Job.info.status == 'SUCCEEDED') {
-                    clearInterval(statusUpdater);
-                    $('#job-progress').removeClass('progress-bar-animated').removeClass('progress-bar-striped');
-                }
-            });
-    };
-
-    var updateLaunchStatus = function() {
-      $http.get('/api/status/cluster/' + vm.ClusterDetails.id)
-        .then(function(res) {
-          var cluster = JSON.parse(res.data);
-          cluster = cluster.cluster;
-          vm.ClusterDetails.status = cluster.status;
-
-          if (vm.ClusterDetails.status === 'Active') {
-            clearInterval(statusUpdater);
-            $('#cluster-progress').removeClass('progress-bar-animated').removeClass('progress-bar-striped');
-
-            var job_type = '';
-
-            switch (vm.Cluster.Plugin) {
-                case 'vanilla':
-                    job_type = 'MapReduce';
-                    break;
+            if (!vm.Cluster.Name.length && !vm.ClusterFT.Name.length) {
+                vm.BinaryUrl = 'Upload aborted. No cluster name specified.'
+                return;
             }
 
-            // Start Job Creation Cycle
+            // Selecting between 'Cluster from Scratch' or 'Cluster from Template' Name
+            var containerName = vm.Cluster.Name.length ? vm.Cluster.Name : vm.ClusterFT.Name;
+
+            var file = $('#upload-input').get(0).files[0];
+            vm.BinaryFileName = file.name;
+
+            if (file) {
+                var formData = new FormData();
+                formData.append('uploads[]', file, containerName + '/' + file.name);
+
+                $.ajax({
+                    url: '/api/upload/binary',
+                    type: 'POST',
+                    data: formData,
+                    processData: false,
+                    contentType: false,
+                    success: function (data) {
+                        vm.BinaryUrl = data;
+                        $scope.$apply();
+                    }
+                });
+            }
+        };
+
+        vm.CreateJob = function () {
             var job = {
-                'job_type': job_type,
+                'job_type': 'MapReduce',
                 'container_name': vm.Cluster.Name,
                 'cluster_id': vm.ClusterDetails.id,
-                'input_sources': vm.InputFiles,
+                'input_sources': vm.SelectedFiles,
                 'binary_url': vm.Cluster.Name + '/' + vm.BinaryFileName
             };
 
             $http.post('/api/create/data_job', job)
               .then(function (res) {
                   vm.Job = res.data.job_execution;
-                  statusUpdater = setInterval(updateJobStatus, 5000);
+                  statusUpdater = setInterval(updateJobStatus, 1000);
               });
-          }
-        });
-    };
+        };
 
-    $http.get('/api/list/servers')
-      .then(function(res) {
-        vm.ServerList = res.data;
-        var flavors = {};
-        var plugins = {};
-        var keypairs = {};
-        var networks = {};
+        vm.VerifyClusterCount = function () {
+            if (vm.Cluster.InstanceCount <= 0) {
+                vm.Cluster.InstanceCount = 1;
+                vm.Cluster.NodeCount = 0;
+            } else if (vm.Cluster.InstanceCount === undefined) {
+                vm.Cluster.NodeCount = 0;
+            } else {
+                vm.Cluster.NodeCount = vm.Cluster.InstanceCount - 1;
+            }
+        };
 
-        $http.get('/api/list/flavors')
-          .then(function(res) {
-              vm.Flavors = res.data;
+        vm.LaunchCluster = function () {
 
-            $http.get('/api/list/plugins')
-              .then(function(res) {
-                var resp = JSON.parse(res.data);
-                vm.Plugins = resp.plugins;                
+            var launchClusterPayload = {
+                'plugin_name': vm.Cluster.Plugin,
+                'name': vm.Cluster.Name,
+                'count': vm.Cluster.InstanceCount - 1,
+                'user_keypair_id': vm.Cluster.KeyPair,
+                'flavor': vm.Cluster.Flavor,
+                'network': vm.Cluster.Network
+            };
+
+            $http.post('/api/launch', launchClusterPayload)
+              .then(function (res) {
+                  if (res.data.error_name !== undefined) {
+                      vm.LaunchSuccess = false;
+                      vm.ClusterDetails = res.data;
+                  } else {
+                      vm.LaunchSuccess = true;
+                      vm.ClusterDetails = res.data.cluster;
+
+                      statusUpdater = setInterval(updateLaunchStatus, 5000);
+                  }
               });
+        };
 
-            $http.get('/api/list/keypairs')
+        vm.LaunchFT = function () {
+            var launchClusterPayload = {
+                'plugin_name': vm.SelectedClusterTemplate.plugin_name,
+                'template_id': vm.SelectedClusterTemplate.id,
+                'name': vm.ClusterFT.Name,
+                'user_keypair_id': vm.ClusterFT.KeyPair,
+                'network': vm.ClusterFT.Network
+            };
+
+            $http.post('/api/create/cluster_from_template', launchClusterPayload)
+              .then(function (res) {
+                  if (res.data.error_name !== undefined) {
+                      vm.LaunchSuccess = false;
+                      vm.ClusterDetails = res.data;
+                  } else {
+                      vm.LaunchSuccess = true;
+                      vm.ClusterDetails = res.data.cluster;
+
+                      statusUpdater = setInterval(updateLaunchStatus, 5000);
+                  }
+              });
+        }
+
+        var updateJobStatus = function () {
+            $http.get('/api/status/job/' + vm.Job.id)
                 .then(function (res) {
-                    keypairs = res.data;
+                    var resp = JSON.parse(res.data);
+                    vm.Job.info = resp.job_execution.info;
 
-                    $http.get('/api/list/networks')
-                      .then(function (res) {
-                          vm.KeyPairs = keypairs;
-                          vm.Cluster.KeyPair = vm.KeyPairs[0].name;
-
-                          vm.Networks = res.data;
-                          vm.Networks = vm.Networks.splice(1, 1);
-                          vm.Cluster.Network = vm.Networks[0].id;
-                      });
+                    if (vm.Job.info.status == 'SUCCEEDED') {
+                        clearInterval(statusUpdater);
+                        $('#job-progress').removeClass('progress-bar-animated').removeClass('progress-bar-striped');
+                    }
                 });
+        };
 
-            $http.get('/api/list/clusters')
+        var updateLaunchStatus = function () {
+            $http.get('/api/status/cluster/' + vm.ClusterDetails.id)
               .then(function (res) {
-                  vm.Clusters = JSON.parse(res.data).clusters;
+                  var cluster = JSON.parse(res.data);
+                  cluster = cluster.cluster;
+                  vm.ClusterDetails.status = cluster.status;
+
+                  if (vm.ClusterDetails.status === 'Active') {
+                      clearInterval(statusUpdater);
+                      $('#cluster-progress').removeClass('progress-bar-animated').removeClass('progress-bar-striped');
+
+                      var job_type = '';
+
+                      switch (vm.Cluster.Plugin) {
+                          case 'vanilla':
+                              job_type = 'MapReduce';
+                              break;
+                      }
+
+                      // Start Job Creation Cycle
+                      var job = {
+                          'job_type': job_type,
+                          'container_name': vm.Cluster.Name,
+                          'cluster_id': vm.ClusterDetails.id,
+                          'input_sources': vm.InputFiles,
+                          'binary_url': vm.Cluster.Name + '/' + vm.BinaryFileName
+                      };
+
+                      $http.post('/api/create/data_job', job)
+                        .then(function (res) {
+                            vm.Job = res.data.job_execution;
+                            statusUpdater = setInterval(updateJobStatus, 5000);
+                        });
+                  }
               });
+        };
 
-            $http.get('/api/list/cluster_templates')
-              .then(function (res) {
-                  vm.ClusterTemplates = JSON.parse(res.data).cluster_templates;
-              });
+        $http.get('/api/list/servers')
+          .then(function (res) {
+              vm.ServerList = res.data;
+              var flavors = {};
+              var plugins = {};
+              var keypairs = {};
+              var networks = {};
 
-            $http.get('/api/container/list')
-              .then(function (res) {
-                  vm.ContainerObjects = res.data;
-              })
+              $http.get('/api/list/flavors')
+                .then(function (res) {
+                    vm.Flavors = res.data;
+
+                    $http.get('/api/list/plugins')
+                      .then(function (res) {
+                          var resp = JSON.parse(res.data);
+                          vm.Plugins = resp.plugins;
+                      });
+
+                    $http.get('/api/list/keypairs')
+                        .then(function (res) {
+                            keypairs = res.data;
+
+                            $http.get('/api/list/networks')
+                              .then(function (res) {
+                                  vm.KeyPairs = keypairs;
+                                  vm.Cluster.KeyPair = vm.KeyPairs[0].name;
+
+                                  vm.Networks = res.data;
+                                  vm.Networks = vm.Networks.splice(1, 1);
+                                  vm.Cluster.Network = vm.Networks[0].id;
+                              });
+                        });
+
+                    $http.get('/api/list/clusters')
+                      .then(function (res) {
+                          vm.Clusters = JSON.parse(res.data).clusters;
+                      });
+
+                    $http.get('/api/list/cluster_templates')
+                      .then(function (res) {
+                          vm.ClusterTemplates = JSON.parse(res.data).cluster_templates;
+                      });
+
+                    $http.get('/api/container/list')
+                      .then(function (res) {
+                          vm.ContainerObjects = res.data;
+                      })
+                });
           });
-      });
-  }
+    }
 }());
